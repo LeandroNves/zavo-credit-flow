512538e27fa7d86a3b5d14ebdf3d61935b1719c9
LeandroNves <leandro.naves123@gmail.com>
2026-05-06 10:23:17 -0300

    att api 1

--- src/pages/admin/AdminManageContract.tsx
+++ src/pages/admin/AdminManageContract.tsx
@@ -30,6 +30,7 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import { useContractsData } from "@/contexts/ContractsDataContext";
 import { contractNumeroForInput } from "@/lib/contractNumero";
 import type { Parcela } from "@/data/mockData";
